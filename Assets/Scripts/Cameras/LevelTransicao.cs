3d5b1b0b3dfc4d11ea479c4aa4ee1c818d02db4d
Willian Chiquinato <willianchiquinato@hotmail.com>
2025-07-10 00:21:09 -0300

    Level Trasicao ajustes

--- Assets/Scripts/Cameras/LevelTransicao.cs
+++ Assets/Scripts/Cameras/LevelTransicao.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[DefaultExecutionOrder(-1000)]
 public class LevelTransicao : MonoBehaviour
 {
     public Animator animator;
